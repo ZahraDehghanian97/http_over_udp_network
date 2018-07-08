1f5d30ad9f2b39ad46cb285ebae98918e8b37968
Mahin Mirshams <m.mirshams@aut.ac.ir>
2018-07-08 11:51:36 +0430

    dns query sending and receiving typeA added

--- client.py
+++ client.py
@@ -3,87 +3,45 @@ import select
 
 
 # function section
-def reliable_send(message):
-    counter = 0
-    global received
-    while counter < 15:
-        if received == 0:
-            result = receive()
-            print(received)
-        if received == 1:
-            if parity(message):
-                counter = 15
-                show_result(result)
-                return True
-            else:
-                received = 2
-        if received == 2:
-            send(MESSAGE)
-            counter += 1
-
-    if counter == 15 and received == 2:
-        print("proxy is not ready to answer")
-        return False
 
 
-def parity(message):
-    # must be implement
-    return True
-
-
-def send(message):
+def send_dns(message):
     global received
     print("send packet")
-    print("UDP target IP:", UDP_IP_s)
-    print("UDP target port:", UDP_PORT_s)
+    print("DNS target IP:", TCP_IP)
+    print("DNS target port:", TCP_PORT)
+    print("DNS target name:", TCP_Target)
     print("message:", message)
-    sock_send.sendto(bytes(message, "utf-8"), (UDP_IP_s, UDP_PORT_s))
-    received = 0
+    newmsg = bytes(DNS_type + "*" + TCP_IP + "*" + TCP_Target + "*" + MESSAGE, 'utf-8')
+    s.connect((TCP_IP, TCP_PORT))
+    s.send(newmsg)
 
 
-def receive():
+def receive_dns():
     global received
     print("client waiting for answer ...")
-    ready = select.select([sock_receive], [], [], 1)
-    if ready[0]:
-        receive_data, addr = sock_receive.recvfrom(1024)  # buffer size is 1024 bytes
-        print("client receive message ")
-        received = 1
-        assert isinstance(receive_data, object)
-        return receive_data
-    else:
-        received = 2
-        print("time out ")
-        return
+
+    data = s.recv(BUFFER_SIZE)
+    s.close()
+    show_result_dns(data)
 
 
-def show_result(message):
+def show_result_dns(message):
     print("received message:", message)
 
 
-# send part initiation
-UDP_IP_s = "127.0.0.1"  # "185.211.88.22"
-UDP_PORT_s = 5005
-sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
 
-# receive part initiation
-UDP_IP_r = "127.0.0.1"  # "185.211.88.22"
-UDP_PORT_r = 5006
-sock_receive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-sock_receive.bind((UDP_IP_r, UDP_PORT_r))
-sock_receive.setblocking(0)
 
-# code section
-received = 0  # 0 just send    1 receive ok   2 time out
-MESSAGE = "GET / HTTP/1.0\r\n\r\n"
-if len(MESSAGE) > 6500 :
-    callSend = len(MESSAGE)/6500 + 1
-    fragment = 1 ; # 1 moreFragment    0 o.w
-for x in range (0,callSend):
-    start = x*6500
-    end = (x+1)*6500
-    if x==callSend :
-        fragment = 0 ;
-    FragmentedMESSAGE = str(fragment)+'*'+ MESSAGE[start,end]
-    reliable_send(FragmentedMESSAGE)
-# parity fragment ip/port/split dns
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+TCP_Target = 'aut.ac.ir'
+DNS_type = 'Aname'
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, World!"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+send_dns(MESSAGE)
+
+receive_dns()
